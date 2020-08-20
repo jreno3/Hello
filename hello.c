41f17ebd51fe584d7e9e305bf96c6da868c3a84f
jreno3 <69804249+jreno3@users.noreply.github.com>
2020-08-20 09:40:38 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jeremiah Reno
+ *         jreno3@cse.unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jeremiah Reno!\n");
+
+  return 0;
+}
