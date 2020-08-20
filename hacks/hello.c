2b17ddd29a944650402e2c232b713f3a2c9778b1
jreno3 <69804249+jreno3@users.noreply.github.com>
2020-08-20 10:44:11 -0500

    Add files via upload

---
+++ hacks/hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Jeremiah Reno
+ *         jreno3@huskers.unl.edu
+ * Date: 2020/08/17
+ *
+ * Hack 1.0 assignment
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jeremiah Reno!\n");
+  printf("Major: Computer Science\n");
+
+
+
+  return 0;
+}
